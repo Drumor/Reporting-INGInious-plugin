ce05722d58c4c523f365cab565b85665081eb005
Drumor <ludovic.taffin@uclouvain.be>
2019-09-30 16:33:52 +0200

    adding second chart

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #   @author 2019 Ludovic Taffin
@@ -15,6 +16,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #   Reporting plugin for INGInious
+import json
 import os
 
 import logging
@@ -51,8 +53,6 @@ def init(plugin_manager, _, _2, config):
                 key=lambda k: k[1][0] if k[1] is not None else "")
 
             tasks = c.get_tasks()
-            self._logger.info(users)
-            self._logger.info(tasks)
 
             self._logger.info("Rendering selection page")
             return self.template_helper.get_custom_renderer(PATH_TO_PLUGIN + "/templates/").reporting_index(users,
@@ -61,11 +61,38 @@ def init(plugin_manager, _, _2, config):
         def POST(self, courseID):
             """POST REQUEST - Allows display of the diagram"""
             self._logger = logging.getLogger("inginious.webapp.plugins.reporting")
-            dicgrade = {}
+            course = self.course_factory.get_course(courseID)
             student_ids = []
             task_ids = []
-            evaluated_submissions = []
             x = web.input()
+            for key in x:
+                if key.startswith("student"):
+                    student_ids.append(x[key])
+                else:
+                    task_ids.append(x[key])
+
+            student_ids = (str(student_ids)).encode("ascii").decode()
+            task_ids = ",".join(task_ids)
+            return self.template_helper.get_custom_renderer(PATH_TO_PLUGIN + "/templates/") \
+                .reporting_chart(course, student_ids, task_ids)
+
+    class Diagram1Page(INGIniousAdminPage):
+        def POST(self,courseID):
+            self._logger = logging.getLogger("inginious.webapp.plugins.reporting")
+            dicgrade = {}
+            data = web.input()
+
+            student_ids = data["student_ids"].replace("&#39;", "")
+            student_ids = student_ids.replace("[", "")
+            student_ids = student_ids.replace("]", "")
+            student_ids = student_ids.replace(" ", "")
+            student_ids= student_ids.split(",")
+            task_ids = data["task_ids"].replace("&#39;", "")
+            task_ids = task_ids.replace("[", "")
+            task_ids = task_ids.replace("]", "")
+            task_ids = task_ids.replace(" ", "")
+            task_ids = task_ids.split(",")
+            evaluated_submissions = []
 
             def students_per_grade(grade_table):
                 for grade in grade_table:
@@ -74,17 +101,10 @@ def init(plugin_manager, _, _2, config):
                     dicgrade[grade] = 1 if grade not in dicgrade else dicgrade[grade] + 1
                 return dicgrade
 
-            for key in x:
-                if key.startswith("student"):
-                    student_ids.append(x[key])
-                else:
-                    task_ids.append(x[key])
-
-            self._logger.info(student_ids)
-            self._logger.info(task_ids)
-
             for stud_id in student_ids:
                 for task_id in task_ids:
+                    self._logger.info(stud_id)
+                    self._logger.info(task_id)
                     submissions = list(self.database.submissions.find(
                         {"courseid": courseID, "taskid": task_id, "username": stud_id}).sort(
                         [("submitted_on", pymongo.DESCENDING)]))
@@ -93,9 +113,46 @@ def init(plugin_manager, _, _2, config):
                     else:
                         pass
             table_stud_per_grade = students_per_grade(evaluated_submissions)
+            return json.dumps(table_stud_per_grade)
+
+    class Diagram2Page(INGIniousAdminPage):
+        def POST(self,courseID):
             course = self.course_factory.get_course(courseID)
-            return self.template_helper.get_custom_renderer(PATH_TO_PLUGIN + "/templates/") \
-                .reporting_chart(course, table_stud_per_grade)
+            self._logger = logging.getLogger("inginious.webapp.plugins.reporting")
+            data = web.input()
+            task_ids = data["task_ids"].replace("&#39;", "")
+            task_ids = task_ids.replace("[", "")
+            task_ids = task_ids.replace("]", "")
+            task_ids = task_ids.replace(" ", "")
+            task_ids = task_ids.split(",")
+            tasks_data = {}
+            tasks_data["nstuds"] = len(self.user_manager.get_course_registered_users(course, False))
+            for task_id in task_ids:
+                data = list(self.database.user_tasks.aggregate(
+                    [
+                        {
+                            "$match":
+                                {
+                                    "courseid": courseID,
+                                    "taskid":task_id,
+                                    "username": {"$in": self.user_manager.get_course_registered_users(course, False)}
+                                }
+                        },
+                        {
+                            "$group":
+                                {
+                                    "_id": "$taskid",
+                                    "viewed": {"$sum": 1},
+                                    "attempted": {"$sum": {"$cond": [{"$ne": ["$tried", 0]}, 1, 0]}},
+                                    "succeeded": {"$sum": {"$cond": ["$succeeded", 1, 0]}}
+                                }
+                        }
+                ]))
+                tasks_data[task_id] = data
+            self._logger.info(tasks_data)
+            return json.dumps(tasks_data)
 
     plugin_manager.add_page("/admin/([^/]+)/reporting/", ReportingPage)
+    plugin_manager.add_page("/admin/([^/]+)/reporting/diag1", Diagram1Page)
+    plugin_manager.add_page("/admin/([^/]+)/reporting/diag2", Diagram2Page)
     plugin_manager.add_hook('course_admin_menu', add_admin_menu)
